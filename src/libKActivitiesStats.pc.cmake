3a24cde223aeeaf3b50dc3054d9ba12eb39a0e0e
Christophe Giboudeaux <christophe@krop.fr>
2017-11-29 09:23:14 +0100

    Start fixing libKActivitiesStats.pc:
    - Add a working version string,
    - Require Qt5Core,
    - Use the correct library name when linking
    
    CCBUG: 386933

--- src/libKActivitiesStats.pc.cmake
+++ src/libKActivitiesStats.pc.cmake
@@ -6,7 +6,7 @@ includedir=${INCLUDE_INSTALL_DIR}
 Name: libKActivitiesStats
 Description: libKActivitiesStats is a C++ library for using KDE activities
 URL: http://www.kde.org
-Requires:
-Version: ${KACTIVITIESSTATS_LIB_VERSION_STRING}
-Libs: -L${LIB_INSTALL_DIR} -lKActivitiesStats
+Requires: Qt5Core
+Version: ${KACTIVITIESSTATS_VERSION}
+Libs: -L${LIB_INSTALL_DIR} -lKF5ActivitiesStats
 Cflags: -I${INCLUDE_INSTALL_DIR}
