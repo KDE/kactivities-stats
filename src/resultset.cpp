KDE/kactivities-stats

commit bdba31fac9993047321d1b7652137379f28befed

Add Date term to KActivities Stats to filter on resource event date
Summary:
Add a Term Date to Query to filter on ResourceEvent start column when querying for resources.
Add two special cases : today and yesterday

Test Plan:
Manual through the test app
autotest

Reviewers: ivan, #frameworks

Reviewed By: ivan

Subscribers: kde-frameworks-devel

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D22717

src/resultset.cpp
@@ -179,6 +179,21 @@ public:
         return QStringLiteral("mimetype LIKE '") + Common::starPatternToLike(mimetype) + QStringLiteral("' ESCAPE '\\'");
     }
 
+    QString dateClause(QDate date) const {
+        return QStringLiteral("DATE(re.start, 'unixepoch') = '") +
+                               date.toString(Qt::ISODate) + QStringLiteral("' ");
+    }
+
+    QString resourceEventJoinClause() const {
+        return QStringLiteral(R"sql(
+            LEFT JOIN
+                ResourceEvent re
+                ON  from_table.targettedResource = re.targettedResource
+                AND from_table.usedActivity      = re.usedActivity
+                AND from_table.initiatingAgent   = re.initiatingAgent
+        )sql");
+    }
+
     /**
      * Transforms the input list's elements with the f member method,
      * and returns the resulting list
@@ -226,7 +241,6 @@ public:
               : QString()
             );
 
-
         // WHERE clause for filtering on agents
         QStringList agentsFilter = transformedList(
                 queryDefinition.agents(), &ResultSetPrivate::agentClause);
@@ -243,6 +257,14 @@ public:
         QStringList mimetypeFilter = transformedList(
                 queryDefinition.types(), &ResultSetPrivate::mimetypeClause);
 
+        QString dateColumn = QStringLiteral("1"), resourceEventJoin;
+        // WHERE clause for access date filtering and ResourceEvent table Join
+        if (!queryDefinition.date().isNull()) {
+            dateColumn = dateClause(queryDefinition.date());
+
+            resourceEventJoin = resourceEventJoinClause();
+        }
+
         auto queryString = _query;
 
         queryString.replace(QStringLiteral("ORDER_BY_CLAUSE"), QStringLiteral("ORDER BY $orderingColumn resource ASC"))
@@ -255,6 +277,8 @@ public:
                 .replace(QLatin1String("$activitiesFilter"), activitiesFilter.join(QStringLiteral(" OR ")))
                 .replace(QLatin1String("$urlFilter"), urlFilter.join(QStringLiteral(" OR ")))
                 .replace(QLatin1String("$mimetypeFilter"), mimetypeFilter.join(QStringLiteral(" OR ")))
+                .replace(QLatin1String("$resourceEventJoin"), resourceEventJoin)
+                .replace(QLatin1String("$dateFilter"), dateColumn)
             );
     }
 
@@ -266,32 +290,35 @@ public:
         static const QString queryString =
             QStringLiteral(R"sql(
             SELECT
-                rl.targettedResource as resource
-              , SUM(rsc.cachedScore) as score
-              , MIN(rsc.firstUpdate) as firstUpdate
-              , MAX(rsc.lastUpdate)  as lastUpdate
-              , rl.usedActivity      as activity
-              , rl.initiatingAgent   as agent
-              , COALESCE(ri.title, rl.targettedResource) as title
+                from_table.targettedResource as resource
+              , SUM(rsc.cachedScore)         as score
+              , MIN(rsc.firstUpdate)         as firstUpdate
+              , MAX(rsc.lastUpdate)          as lastUpdate
+              , from_table.usedActivity      as activity
+              , from_table.initiatingAgent   as agent
+              , COALESCE(ri.title, from_table.targettedResource) as title
               , ri.mimetype as mimetype
               , 2 as linkStatus
 
             FROM
-                ResourceLink rl
+                ResourceLink from_table
             LEFT JOIN
                 ResourceScoreCache rsc
-                ON rl.targettedResource = rsc.targettedResource
-                AND rl.usedActivity     = rsc.usedActivity
-                AND rl.initiatingAgent  = rsc.initiatingAgent
+                ON  from_table.targettedResource = rsc.targettedResource
+                AND from_table.usedActivity      = rsc.usedActivity
+                AND from_table.initiatingAgent   = rsc.initiatingAgent
             LEFT JOIN
                 ResourceInfo ri
-                ON rl.targettedResource = ri.targettedResource
+                ON from_table.targettedResource = ri.targettedResource
+
+            $resourceEventJoin
 
             WHERE
                 ($agentsFilter)
                 AND ($activitiesFilter)
                 AND ($urlFilter)
                 AND ($mimetypeFilter)
+                AND ($dateFilter)
 
             GROUP BY resource, title
 
@@ -310,27 +337,30 @@ public:
         static const QString queryString =
             QStringLiteral(R"sql(
             SELECT
-                rsc.targettedResource as resource
-              , SUM(rsc.cachedScore)  as score
-              , MIN(rsc.firstUpdate)  as firstUpdate
-              , MAX(rsc.lastUpdate)   as lastUpdate
-              , rsc.usedActivity      as activity
-              , rsc.initiatingAgent   as agent
-              , COALESCE(ri.title, rsc.targettedResource) as title
+                from_table.targettedResource as resource
+              , SUM(from_table.cachedScore)  as score
+              , MIN(from_table.firstUpdate)  as firstUpdate
+              , MAX(from_table.lastUpdate)   as lastUpdate
+              , from_table.usedActivity      as activity
+              , from_table.initiatingAgent   as agent
+              , COALESCE(ri.title, from_table.targettedResource) as title
               , ri.mimetype as mimetype
               , 1 as linkStatus
 
             FROM
-                ResourceScoreCache rsc
+                ResourceScoreCache from_table
             LEFT JOIN
                 ResourceInfo ri
-                ON rsc.targettedResource = ri.targettedResource
+                ON from_table.targettedResource = ri.targettedResource
+
+            $resourceEventJoin
 
             WHERE
                 ($agentsFilter)
                 AND ($activitiesFilter)
                 AND ($urlFilter)
                 AND ($mimetypeFilter)
+                AND ($dateFilter)
 
             GROUP BY resource, title
 
@@ -351,47 +381,53 @@ public:
             QStringLiteral(R"sql(
             WITH
                 LinkedResourcesResults AS (
-                    SELECT rl.targettedResource as resource
-                         , rsc.cachedScore      as score
-                         , rsc.firstUpdate      as firstUpdate
-                         , rsc.lastUpdate       as lastUpdate
-                         , rl.usedActivity      as activity
-                         , rl.initiatingAgent   as agent
+                    SELECT from_table.targettedResource as resource
+                         , rsc.cachedScore              as score
+                         , rsc.firstUpdate              as firstUpdate
+                         , rsc.lastUpdate               as lastUpdate
+                         , from_table.usedActivity      as activity
+                         , from_table.initiatingAgent   as agent
                          , 2 as linkStatus
 
                     FROM
-                        ResourceLink rl
+                        ResourceLink from_table
 
                     LEFT JOIN
                         ResourceScoreCache rsc
-                        ON  rl.targettedResource = rsc.targettedResource
-                        AND rl.usedActivity      = rsc.usedActivity
+                        ON  from_table.targettedResource = rsc.targettedResource
+                        AND from_table.usedActivity      = rsc.usedActivity
                         AND rl.initiatingAgent   = rsc.initiatingAgent
+
+                    $resourceEventJoin
 
                     WHERE
                         ($agentsFilter)
                         AND ($activitiesFilter)
                         AND ($urlFilter)
                         AND ($mimetypeFilter)
+                        AND ($dateFilter)
                 ),
 
                 UsedResourcesResults AS (
-                    SELECT rsc.targettedResource as resource
-                         , rsc.cachedScore       as score
-                         , rsc.firstUpdate       as firstUpdate
-                         , rsc.lastUpdate        as lastUpdate
-                         , rsc.usedActivity      as activity
-                         , rsc.initiatingAgent   as agent
+                    SELECT from_table.targettedResource as resource
+                         , from_table.cachedScore       as score
+                         , from_table.firstUpdate       as firstUpdate
+                         , from_table.lastUpdate        as lastUpdate
+                         , from_table.usedActivity      as activity
+                         , from_table.initiatingAgent   as agent
                          , 0 as linkStatus
 
                     FROM
-                        ResourceScoreCache rsc
+                        ResourceScoreCache from_table
+
+                    $resourceEventJoin
 
                     WHERE
                         ($agentsFilter)
                         AND ($activitiesFilter)
                         AND ($urlFilter)
                         AND ($mimetypeFilter)
+                        AND ($dateFilter)
                 ),
 
                 CollectedResults AS (

autotests/QueryTest.cpp
@@ -297,6 +297,24 @@ void QueryTest::testFancySyntaxOrderingDefinition()
     }
 }
 
+void QueryTest::testNormalSyntaxDateDefinition()
+{
+    TEST_CHUNK(QStringLiteral("Testing the Date definition"))
+    {
+        auto query = Date::today();
+        QCOMPARE(query.value, QDate::currentDate());
+    }
+    {
+        auto query = Date::yesterday();
+        QDate date = QDate::currentDate();
+        QCOMPARE(query.value, date.addDays(-1));
+    }
+    {
+        auto query = Date(QDate::fromString(QStringLiteral("2019-07-25")));
+        QCOMPARE(query.value, QDate::fromString(QStringLiteral("2019-07-25")));
+    }
+}
+
 void QueryTest::initTestCase()
 {
     // CHECK_CONDITION(isActivityManagerRunning, FailIfTrue);

autotests/QueryTest.h
@@ -44,6 +44,7 @@ private Q_SLOTS:
     void testNormalSyntaxTypeManipulation();
     void testNormalSyntaxActivityManipulation();
     void testNormalSyntaxOrderingManipulation();
+    void testNormalSyntaxDateDefinition();
 
     void testFancySyntaxBasic();
     void testFancySyntaxAgentDefinition();

autotests/ResultSetTest.cpp
@@ -146,6 +146,18 @@ void ResultSetTest::testUsedResources()
         QCOMPARE(result.at(1).resource(), QStringLiteral("/path/high2_act2_kate"));
         QCOMPARE(result.at(2).resource(), QStringLiteral("/path/high3_act1_kate"));
     }
+
+    TEST_CHUNK(QStringLiteral("Getting the used resources filter by Date"))
+    {
+        ResultSet result(UsedResources
+                        | HighScoredFirst
+                        | Agent::any()
+                        | Activity::any()
+                        | Date::fromString(QStringLiteral("2015-01-15"))
+                        );
+
+        QCOMPARE(result.at(0).resource(), QStringLiteral("/path/high1_act1_gvim"));
+    }
 }
 
 void ResultSetTest::initTestCase()
@@ -192,8 +204,17 @@ void ResultSetTest::initTestCase()
             " , ('activity1' , 'gvim'                 , '/path/low3_act1_gvim'  , '0' , '6'   , '-1' , '1421434704')"
             " , ('activity1' , 'kate'                 , '/path/low2_act1_kate'  , '0' , '3'   , '-1' , '1421433266')"
             " , ('activity1' , 'kate'                 , '/path/low1_act1_kate'  , '0' , '2'   , '-1' , '1421433254')")
-
         );
+
+    database->execQuery(
+            QStringLiteral("INSERT INTO  ResourceEvent (usedActivity, initiatingAgent, targettedResource, start, end ) VALUES"
+             "('activity1' , 'gvim'                 , '/path/high1_act1_gvim' , '1421345799', '1421345799')")
+    );
+
+    database->execQuery(
+            QStringLiteral("INSERT INTO  ResourceInfo (targettedResource, title, mimetype, autoTitle, autoMimetype) VALUES"
+             "('/path/high1_act1_gvim', 'high1_act1_gvim', 'text/plain', 1, 1 )")
+    );
 
     // Renaming the activity1 to the current acitivty
     KActivities::Consumer kamd;
@@ -204,6 +225,11 @@ void ResultSetTest::initTestCase()
 
     database->execQuery(
             QStringLiteral("UPDATE ResourceScoreCache SET usedActivity = '")
+                + kamd.currentActivity()
+                + QStringLiteral("' WHERE usedActivity = 'activity1'"));
+
+    database->execQuery(
+            QStringLiteral("UPDATE ResourceEvent SET usedActivity = '")
                 + kamd.currentActivity()
                 + QStringLiteral("' WHERE usedActivity = 'activity1'"));
 

src/query.cpp
@@ -20,6 +20,7 @@
  */
 
 #include "query.h"
+#include <QDate>
 #include <QDebug>
 
 namespace KActivities {
@@ -71,6 +72,7 @@ public:
     QStringList     activities;
     QStringList     urlFilters;
     Terms::Order    ordering;
+    QDate           date;
     int             limit;
     int             offset;
 };
@@ -111,7 +113,8 @@ bool Query::operator== (const Query &right) const
            agents()     == right.agents() &&
            activities() == right.activities() &&
            selection()  == right.selection() &&
-           urlFilters() == right.urlFilters();
+           urlFilters() == right.urlFilters() &&
+           date()       == right.date();
 }
 
 bool Query::operator!= (const Query &right) const
@@ -163,6 +166,11 @@ void Query::setOffset(int offset)
     d->offset = offset;
 }
 
+void Query::setDate(QDate date)
+{
+    d->date = date;
+}
+
 Terms::Order Query::ordering() const
 {
     return d->ordering;
@@ -184,6 +192,11 @@ int Query::offset() const
     return d->offset;
 }
 
+QDate Query::date() const
+{
+    return d->date;
+}
+
 } // namespace Stats
 } // namespace KActivities
 
@@ -200,6 +213,7 @@ QDebug operator<<(QDebug dbg, const KAStats::Query &query)
         << ", " << Agent(query.agents())
         << ", " << Activity(query.activities())
         << ", " << Url(query.urlFilters())
+        << ", " << Date(query.date())
         << ", " << query.ordering()
         << ", Limit: " << query.limit()
         << " }";

src/query.h
@@ -89,6 +89,7 @@ public:
     Terms::Order ordering() const;
     int offset() const;
     int limit() const;
+    QDate date() const;
 
     void setSelection(Terms::Select selection);
 
@@ -99,6 +100,7 @@ public:
     void setOrdering(Terms::Order ordering);
     void setOffset(int offset);
     void setLimit(int limit);
+    void setDate(QDate date);
 
     void clearTypes();
     void clearAgents();
@@ -149,6 +151,11 @@ private:
     inline void addTerm(Terms::Offset offset)
     {
         setOffset(offset.value);
+    }
+
+    inline void addTerm(Terms::Date date)
+    {
+        setDate(date.value);
     }
 
 public:

src/terms.cpp
@@ -76,6 +76,28 @@ Terms::Offset::Offset(int value)
 {
 }
 
+Terms::Date::Date(QDate value)
+    : value(value)
+{
+}
+
+Terms::Date Terms::Date::today()
+{
+    return Date(QDate::currentDate());
+}
+
+Terms::Date Terms::Date::yesterday()
+{
+    auto date = QDate::currentDate();
+    return Date(date.addDays(-1));
+}
+
+Terms::Date Terms::Date::fromString(QString string)
+{
+    auto date = QDate::fromString(string, Qt::ISODate);
+    return Date(date);
+}
+
 Terms::Url Terms::Url::startsWith(const QString &prefix)
 {
     return Url(prefix + QStringLiteral("*"));
@@ -115,6 +137,7 @@ QDEBUG_TERM_OUT(Url,      _.values)
 
 QDEBUG_TERM_OUT(Limit,    _.value)
 QDEBUG_TERM_OUT(Offset,   _.value)
+QDEBUG_TERM_OUT(Date,     _.value)
 
 #undef QDEBUG_TERM_OUT
 

src/terms.h
@@ -31,6 +31,7 @@
 
 #include <QString>
 #include <QStringList>
+#include <QDate>
 
 #include "kactivitiesstats_export.h"
 
@@ -202,6 +203,18 @@ namespace Terms {
         const QStringList values;
 
     };
+
+    /**
+     * On which start access date do you want to filter ?
+     */
+    struct KACTIVITIESSTATS_EXPORT Date {
+        Date(QDate value);
+        static Date today();
+        static Date yesterday();
+        static Date fromString(QString);
+        QDate value;
+    };
+
 } // namespace Terms
 
 } // namespace Stats
@@ -230,6 +243,9 @@ QDebug operator<<(QDebug dbg, const KActivities::Stats::Terms::Limit &limit);
 
 KACTIVITIESSTATS_EXPORT
 QDebug operator<<(QDebug dbg, const KActivities::Stats::Terms::Offset &offset);
+
+KACTIVITIESSTATS_EXPORT
+QDebug operator<<(QDebug dbg, const KActivities::Stats::Terms::Date &date);
 
 #endif // KACTIVITIES_STATS_TERMS_H
 

commit e36497a6734d0b3787c8cd48a2f7f248f290276c

Fix broken SQL query in allResourcesQuery
Summary: The regression was introduced by D22717

Test Plan:
compile
Launch systemsettings
No more logs: `kf5.kactivity.stat: SQL:
    error:  QSqlError` in terminal

Reviewers: ivan, #frameworks

Reviewed By: ivan

Subscribers: kde-frameworks-devel

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D26798

src/resultset.cpp
@@ -419,7 +419,7 @@ public:
                         ResourceScoreCache rsc
                         ON  from_table.targettedResource = rsc.targettedResource
                         AND from_table.usedActivity      = rsc.usedActivity
-                        AND rl.initiatingAgent   = rsc.initiatingAgent
+                        AND from_table.initiatingAgent   = rsc.initiatingAgent
 
                     $resourceEventJoin
 