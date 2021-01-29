82a57638804dbbd8476413c2c230655d70934c3f
Friedrich W. H. Kossebau <kossebau@kde.org>
2021-01-29 03:25:11 +0100

    Use non-deprecated KDEInstallDirs variables also for pkg-config template
    NO_CHANGELOG

--- src/libKActivitiesStats.pc.cmake
+++ src/libKActivitiesStats.pc.cmake
@@ -1,12 +1,12 @@
 prefix=${CMAKE_INSTALL_PREFIX}
-exec_prefix=${BIN_INSTALL_DIR}
-libdir=${LIB_INSTALL_DIR}
-includedir=${INCLUDE_INSTALL_DIR}
+exec_prefix=${KDE_INSTALL_BINDIR}
+libdir=${KDE_INSTALL_LIBDIR}
+includedir=${KDE_INSTALL_INCLUDEDIR}
 
 Name: libKActivitiesStats
 Description: libKActivitiesStats is a C++ library for using KDE activities
 URL: http://www.kde.org
 Requires: Qt5Core
 Version: ${KACTIVITIESSTATS_VERSION}
-Libs: -L${LIB_INSTALL_DIR} -lKF5ActivitiesStats
-Cflags: -I${INCLUDE_INSTALL_DIR}
+Libs: -L${KDE_INSTALL_LIBDIR} -lKF5ActivitiesStats
+Cflags: -I${KDE_INSTALL_INCLUDEDIR}
