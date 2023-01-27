ac74660377c1d247754b7c53994c00b9ff165c5e
Volker Krause <vkrause@kde.org>
2023-01-27 16:06:11 +0100

    Remove Qt 5 support

--- src/libKActivitiesStats.pc.cmake
+++ src/libKActivitiesStats.pc.cmake
@@ -6,7 +6,7 @@ includedir=${KDE_INSTALL_INCLUDEDIR}
 Name: libKActivitiesStats
 Description: libKActivitiesStats is a C++ library for using KDE activities
 URL: http://www.kde.org
-Requires: Qt${QT_MAJOR_VERSION}Core
+Requires: Qt6Core
 Version: ${KACTIVITIESSTATS_VERSION}
 Libs: -L${KDE_INSTALL_LIBDIR} -lKF6ActivitiesStats
 Cflags: -I${KDE_INSTALL_INCLUDEDIR}
